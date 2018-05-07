efb1e0fac3a3bcf124dee26a85d4aedad2ac4fda
arturbarbosa1 <35673399+arturbarbosa1@users.noreply.github.com>
2018-05-07 01:33:04 -0400

    Update State.java

--- State.java
+++ State.java
@@ -17,3 +17,19 @@ public class State {
 	private int id;
 	HashMap<String, Set<State>> transitions = new HashMap<String, Set<State>>();
 	private static int nextStateNum = 0;
+	
+	/**
+	 * Returns a new state with unique next id
+	 * @return A State object with unique id
+	 */
+	public static State getNewState() {
+		return new State(nextStateNum++);
+	}
+	
+	/**
+	 * Class constructor that creates a new State with given id.
+	 * @param id the id of the state
+	 */
+	private State(int id) {
+		this.id  = id;
+	}
