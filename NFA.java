760d7412d9b9276099e081f91c7da5841de6dbd1
arturbarbosa1 <35673399+arturbarbosa1@users.noreply.github.com>
2018-05-09 19:07:32 -0400

    Update NFA.java

--- NFA.java
+++ NFA.java
@@ -1,1 +1,14 @@
-
+/**
+   * file: NFA.java
+   * author: Artur Barbosa
+   * course: CMPT 432
+   * assignment: Grephy 
+   * due date: 5/7/2018
+   * Version 1.4
+   * 
+   * This file contains the NFA.java file for my grep project.
+   *
+   */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
